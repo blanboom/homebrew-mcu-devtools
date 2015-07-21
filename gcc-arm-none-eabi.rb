70bc015f3a8f5270bc7af3bf45152a050f0c3f1e
Blanboom <i@blanboom.org>
2015-07-21 13:03:26 +0800

    Update to 4.9-2015q2

--- gcc-arm-none-eabi.rb
+++ gcc-arm-none-eabi.rb
@@ -1,9 +1,9 @@
 
 class GccArmNoneEabi < Formula
   homepage "https://launchpad.net/gcc-arm-embedded"
-  url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q1-update/+download/gcc-arm-none-eabi-4_9-2015q1-20150306-mac.tar.bz2"
-  version "4_9-2015q1"
-  sha1 "da07fd4edc09da8748b3a61252eed793059c138f"
+  url "https://launchpad.net/gcc-arm-embedded/4.9/4.9-2015-q2-update/+download/gcc-arm-none-eabi-4_9-2015q2-20150609-mac.tar.bz2"
+  version "4_9-2015q2"
+  sha256 '48841185eefa482f0338059dd779b3edf832521ccd05811d557ef4a2807b8284'
 
   def install
     bin.install Dir['bin/*']
